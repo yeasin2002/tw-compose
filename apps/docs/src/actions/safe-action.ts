73c82c52659f0b53afc5ca8c05d810e16e2c790c
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-02-06 17:41:57 +0600

    chore: removed doc

--- apps/docs/src/actions/safe-action.ts
+++
@@ -1,4 +0,0 @@
-import { createSafeActionClient } from "next-safe-action";
-
-// Create the client with default options.
-export const actionClient = createSafeActionClient();
--- apps/docs/src/components/styleglide-provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { KitViewProvider } from "@styleglide/kit-view-provider";
-
-export function StyleGlideProvider() {
-  return <KitViewProvider />;
-}
