389ed2e4a1d54349bcbe475bbab014155bac3fa7
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-02-08 00:36:04 +0600

    chore: removed example

--- examples/vite-react/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
-import clsExtended from "cls-extended/adapters/vite";
-import { defineConfig } from "vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), clsExtended()],
-});
